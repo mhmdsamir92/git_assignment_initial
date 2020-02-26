526f31d979489fef21173b44c21d9fdaadd09fcd
Abelrahman Reda <boodycat09@gmail.com>
2020-02-26 17:00:47 +0200

    add reda subscriber

--- src/redaSubscriber.java
+++ src/redaSubscriber.java
@@ -2,7 +2,8 @@ import java.lang.Math.*;
 public class redaSubsriber implements ISubscriber {
     @Override
     public void notifySubscriber(String input) {
-        System.out.println("Hello, I am a reda subscriber and I am notified with sphere radius :  " + 4/3 *Math.PI*Double.parseDouble(input) );
+     
+        System.out.println("Hello, I am a reda subscriber and I am notified with sphere radius :  " + (4.0/3.0 )*(Math.PI)* (Math.pow(Double.parseDouble(input),3)));
     
     }
 }
