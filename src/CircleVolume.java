694d43f8aacbb428c12edf43e3ea6480aea12c1c
Mina Botros <36329091+Mina1998@users.noreply.github.com>
2020-02-27 09:43:04 +0200

    First Version of the task

---
+++ src/CircleVolume.java
@@ -0,0 +1,10 @@
+
+public class CircleVolume implements ISubscriber {
+
+	@Override
+	public void notifySubscriber(String input) {
+		// TODO Auto-generated method stub
+		System.out.println("0");
+	}
+
+}
