b840223fb9fc519a5a163c2eeef6b5046eaabf7f
AhmedAyman16899 <36383074+AhmedAyman16899@users.noreply.github.com>
2020-02-27 22:42:55 +0200

    added CircleArea 20170012

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
 			new SphereArea(),
+			new CircleArea(),
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
