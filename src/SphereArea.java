db2094b80bd9f533122c632977a22c25b4863c3f
Hussein-Hossam-Idris <36144142+Hussein-Hossam-Idris@users.noreply.github.com>
2020-02-28 13:27:12 +0200

    Create SphereArea.java

---
+++ src/SphereArea.java
@@ -0,0 +1,12 @@
+
+public class SphereArea {
+	
+	public SphereArea() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	void calcSphereArea(double r){
+		double area = 4*3.14*(r*r);
+		System.out.println(area);
+	}
+}
