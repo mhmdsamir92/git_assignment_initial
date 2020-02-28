d2aff5f55e3676c92b02d24cf5ff0da706d4d9c6
Mohamed Sameh <mohamed.sameh372@yahoo.com>
2020-02-28 18:09:14 +0200

    New Version. New Feature Added (Sphere Circumference). By Mohamed Sameh 20170232.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
+			new ReallySimpleSubscriber(),new SphereCircumference()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
