7c02f0c950847bce18257447b9fb44d81bf6b0c7
karimsalah270 <36330452+karimsalah270@users.noreply.github.com>
2020-02-28 16:36:47 +0200

    Add files via upload

---
+++ SphereVolume.java
@@ -0,0 +1,13 @@
+
+public class SphereVolume implements ISubscriber{
+
+	@Override
+	public void notifySubscriber(String input) {
+		// TODO Auto-generated method stub
+		double radius = Double.parseDouble(input);
+		double Volume = (4*22*radius*radius*radius) / (3*7);
+		System.out.println("Volume=" + Volume);
+	}
+	
+}	
+	
