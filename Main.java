803ae3f08189bee7f70e7660bea6ce1609a553ba
mai41 <36419468+mai41@users.noreply.github.com>
2020-02-29 00:43:58 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	private static ISubscriber subscribers [] = {
+			new TwoPowerN(),
+			new Circle(),
+			new Shperevolume(),
+                        new CircleCircumference()
+	};
+	public static void main(String[] args) {
+		Topic mathTopic = new Topic();
+		for (ISubscriber sub : subscribers) {
+			mathTopic.addSubscriber(sub);
+		}
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		mathTopic.dispatchEvent(input);
+	}
+}
