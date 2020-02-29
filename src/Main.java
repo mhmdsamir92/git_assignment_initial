fde60fb4beeb1d0edacc37791a4945b4cbcb5c66
abdelrahmanIEl-Batal <abdelrahman.elbatal19@gmail.com>
2020-02-29 23:56:11 +0200

    Added SphereArea, Name:Abdelrahman Ibrahim, ID:20170139

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 			new Circle(),
 			new Shperevolume(),
 			new CircleCircumference(),
-			new Lucas()
+			new Lucas(),
+			new SphereArea()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
--- src/SphereArea.java
+++ src/SphereArea.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
 
-public class SphereArea {
+public class SphereArea implements ISubscriber{
+
+    @Override
+    public void notifySubscriber(String input) {
+        int intInput = Integer.parseInt(input);
+        double res = 4.0 * 3.14159265359 * intInput * intInput;
+        System.out.print("The sphere area is : " + res + "\n");
+    }
 }
