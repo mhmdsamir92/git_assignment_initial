6929ec6e24f57989583904050eb19366a246ec60
Hussein-Hossam-Idris <36144142+Hussein-Hossam-Idris@users.noreply.github.com>
2020-02-29 02:06:35 +0200

    Update SphereArea.java

--- src/SphereArea.java
+++ src/SphereArea.java
@@ -5,7 +5,7 @@ public class SphereArea implements ISubscriber{
 		// TODO Auto-generated constructor stub
 	}
 	
-	void calcSphereArea(double r){
+	public void calcSphereArea(double r){
 		double area = 4*3.14*(r*r);
 		System.out.println(area);
 	}
