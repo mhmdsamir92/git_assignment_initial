a94d9c4b1477b0f6ad77b5e93c9704620ae4ad5c
Esraa rammadan <43940092+Esraaelanany@users.noreply.github.com>
2020-03-01 00:31:59 +0200

    Update Main.java
    power function

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 			new ReallySimpleSubscriber(),
 			new LucasSeries(),
 			new CalculateArea()
-			ner Power(),	
+			new Power(),	
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
