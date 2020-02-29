fa193faed19508a0a3a4b14ef07332b1259a9beb
mariam4o <36135609+mariam4o@users.noreply.github.com>
2020-02-29 16:15:43 +0200

    Update MultiplicationSeries.java

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -6,12 +6,16 @@ public class MultiplicationSeries implements ISubscriber {
 	@Override
 
 	public void notifySubscriber(String input) {
+		int x;
+		int multiply=1;
 
-		int x = Integer.parseInt(input);
+		 x = Integer.parseInt(input);
+		for(int i = 1 ; i < x+1 ; i++){
+		multiply = multiply*i;
+		}
 
-		System.out.println("Hello i'm a Multiplication Series subscriber and "
-
-				+ "i'm notified with " + input);
+		System.out.println("Hello i'm a Multiplication Series subscriber and " + "i'm notified with " + input);
+		 System.out.println("The multiplication of The Series is till "+ x +" is: "+ multiply );
 
 		
 
