5702d3ac9260aff451df0c491648555086740b52
mariam4o <36135609+mariam4o@users.noreply.github.com>
2020-02-29 16:01:11 +0200

    Update MultiplicationSeries.java

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -1,1 +1,22 @@
 
+public class MultiplicationSeries implements ISubscriber {
+
+
+
+	@Override
+
+	public void notifySubscriber(String input) {
+
+		int x = Integer.parseInt(input);
+
+		System.out.println("Hello i'm a Multiplication Series subscriber and "
+
+				+ "i'm notified with " + input);
+
+		
+
+	}
+
+
+
+}
