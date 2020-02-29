e312f65816ba9ff75109974b431f98c156582364
esraa2367 <36345039+esraa2367@users.noreply.github.com>
2020-03-01 00:15:18 +0200

    I created new class called Sphere_Volume

--- src/Sphere_Volume.java
+++ src/Sphere_Volume.java
@@ -1,4 +1,9 @@
-
-public class Sphere_Volume {
-
+public class Sphere_Volume implements ISubscriber {
+	@Override
+	public void notifySubscriber(String input) {
+		double radius = Double.parseDouble(input);
+		double Sphere_volume = (4/3) * Math.PI * Math.pow(radius, 3);
+		System.out.println("Sphere_volume = " + Sphere_volume);
+	}
 }
+
