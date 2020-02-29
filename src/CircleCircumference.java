5d9d29f9ff33cdc0f3ed19bd4961ac8db5fee225
Fatima <44647655+fatima196@users.noreply.github.com>
2020-02-29 18:38:10 +0200

    update 1 in class CircleCircumference

--- src/CircleCircumference.java
+++ src/CircleCircumference.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package SW2_git_assignment_CircleCircumference;
-
 import java.util.Scanner;
 
-/**
- *
- * @author lenovo
- */
 public class CircleCircumference implements ISubscriber {
     
      @Override
