43985e0716d3ec52cacde377440c7890bcd7fb3a
MayadaMohamed11 <36480871+MayadaMohamed11@users.noreply.github.com>
2020-02-29 21:40:26 +0200

    Update CircleVolum.java

--- CircleVolum.java
+++ CircleVolum.java
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package circlevolum;
-
-import java.util.Scanner;
-
-/**
- *
- * 
- */
 public class CircleVolum implements ISubscriber{
     public void notifySubscriber(String input){
         Integer radius = Integer.parseInt(input);
