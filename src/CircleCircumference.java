c5ac0225b55453fa36e978a6baee31a7590919e4
Fatima <44647655+fatima196@users.noreply.github.com>
2020-03-02 00:59:17 +0200

    Change type of input to integer.

--- src/CircleCircumference.java
+++ src/CircleCircumference.java
@@ -10,15 +10,15 @@ public class CircleCircumference implements ISubscriber {
 	}
         
     void display(String input){ // accept input and check if valid or not then call get_CircleCircumference and display the result.
-        double radius=0;
-        try{ //check if the input can converted to double or not ..
-        radius=Double.valueOf(input); // convert input String to double.
+        int radius=0;
+        try{ //check if the input can converted to integer or not ..
+        radius=Integer.parseInt(input); // convert input String to integer.
+	if (radius<0) {System.out.println(" Sorry,You enterd invalid radius. "); return; }  //its invalid if the input radius < 0. 
         }catch (NumberFormatException e) {
             System.out.println(" invalid input ");
             System.out.println("  "+ e.getMessage());
             return;
-        }
-        if (radius<0) {System.out.println(" Sorry,You enterd invalid radius. "); return; } //its invalid if the input radius < 0. 
+        } 
         double circumference = get_CircleCircumference(radius);
         System.out.println(" Circumference of a Circle = " + circumference);
     }
