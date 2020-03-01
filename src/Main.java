ad20400cccc8d6eb9e67b851f6c01a1545d3b5f9
H <mickeymaher50@gmail.com>
2020-03-02 00:37:03 +0200

    hekmat branch 20170096

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
       while (true) {
           ThreadSubscriber subscribers[] = {
-                  new SphereCircumference()
+                  new SphereCircumference(),
+                  new twoPowerN()
           };
 
           Topic mathTopic = new Topic();
