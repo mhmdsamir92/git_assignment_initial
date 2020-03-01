68367dba07ae50e1aea9201a23614165f46024a0
3bdallh <you@example.com>
2020-03-01 18:17:29 +0200

    fff

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
 	};
+jajajjaaj
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
