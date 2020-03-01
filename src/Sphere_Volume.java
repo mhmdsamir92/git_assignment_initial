84b2a42ea639e74c31510bba5800761a8e027a00
Ahmed01151526059 <44085402+Ahmed01151526059@users.noreply.github.com>
2020-03-01 19:27:57 +0200

    circle volume

--- src/Sphere_Volume.java
+++
@@ -1,9 +0,0 @@
-public class Sphere_Volume implements ISubscriber {
-	@Override
-	public void notifySubscriber(String input) {
-		double radius = Double.parseDouble(input);
-		double Sphere_volume = (4/3) * Math.PI * Math.pow(radius, 3);
-		System.out.println("Sphere_volume = " + Sphere_volume);
-	}
-}
-
