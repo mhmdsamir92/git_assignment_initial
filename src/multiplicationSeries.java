00f6c544f91335c8909df65c3f01d68221b4c482
youssef Ismail <58638417+yosefAismail@users.noreply.github.com>
2020-03-01 13:40:50 +0200

    Add class Multiplication Series

---
+++ src/multiplicationSeries.java
@@ -0,0 +1,10 @@
+public class multiplicationSeries implements ISubscriber {
+    public void notifySubscriber(String input) 
+    {
+	int number = Integer.ParseInt(input);
+        for(int i=1; i <= number; i++)
+        {
+            System.out.println(number+" * "+i+" = "+number*i);
+        }
+    }
+}
