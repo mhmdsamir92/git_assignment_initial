ce90afde255b669d50dcaf12d33be0316af06f17
omarrsamy <36420358+omarrsamy@users.noreply.github.com>
2020-03-01 22:53:59 +0200

    Update Lucas_Series_20170176.java
    20170176

--- GitAssignment/src/Lucas_Series.java
+++ GitAssignment/src/Lucas_Series.java
@@ -2,22 +2,27 @@ import java.util.ArrayList;
 
 public class Lucas_Series implements ISubscriber {
 
-    public static int getLucasSeries(double x) {
+    private static long a[] =new long [102];
+    public static long getLucasSeries(int x) {
 
         if (x == 0){
             return 2;}
         if (x == 1){
             return 1;}
 
-
-        return (getLucasSeries(x - 1) + getLucasSeries(x - 2) );
-
+        if ( a[x] != -1 ){
+            return a[x];
+        }
+        a[x]=getLucasSeries(x - 1) + getLucasSeries(x - 2);
+        return a[x];
     }
-
 
     @Override
     public void notifySubscriber(String input) {
         // TODO Auto-generated method stub
+        for (int i =0 ; i < 102 ; i++){
+            a[i] = -1;
+        }
         int x = Integer.valueOf(input);
         System.out.println("Lucas Series = " + getLucasSeries(x));
     }
