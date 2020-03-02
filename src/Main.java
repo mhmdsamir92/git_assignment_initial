7ebd82547d88860093846866ebebe1fffe32d4f6
halahamde <36208722+halahamde@users.noreply.github.com>
2020-03-03 01:25:04 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,8 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
-			new SphereVolumeSubscriber(),
-			new MultiplicationSeries(),
 		         new SummationSeries(),
-			new Circlecir()
-		        
+			
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
