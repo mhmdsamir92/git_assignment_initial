f09dd35f26ea9bd950959c699adaa9f4ed70c535
Andrew Emad <andrewemad614@gmail.com>
2020-03-02 20:32:47 +0200

    20170061

--- src/sphereAreaSubscriber.java
+++ src/sphereAreaSubscriber.java
@@ -4,7 +4,7 @@ public class sphereAreaSubscriber implements ISubscriber {
 	@Override
 	public void notifySubscriber(int r) {
 		double area = 4* Math.PI *r * r;
-		System.out.println("Hello, I am really a simple subscriber and I am notified with "+ area);
+		System.out.println("Sphere Area =  "+ area);
 	}
 
 }
