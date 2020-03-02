9610efd9a203f5da352cb8cd50f4e1f2605688e5
esraa2367 <36345039+esraa2367@users.noreply.github.com>
2020-03-02 17:50:10 +0200

    Sphere Volume implementation

--- src/Sphere_Volume.java
+++ src/Sphere_Volume.java
@@ -1,4 +1,17 @@
 
-public class Sphere_Volume {
-
+public class Sphere_Volume implements ISubscriber { 
+	@Override
+	public void notifySubscriber(String input) {
+		
+		double Radius = Double.parseDouble(input);
+		if(Radius < 0)
+		{
+			System.out.println("Radius can not be equal nagative number");
+		}
+		else {
+		double Volume = (4/3) * Math.PI * Math.pow(Radius, 3);
+		System.out.println("Volume = "+ Volume);
+		}
+		
+	}
 }
