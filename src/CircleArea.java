b62da27d4b6ed1f3f56e04c79556c0c18c4bf0cc
Marim-medhat <36182751+Marim-medhat@users.noreply.github.com>
2020-03-02 10:39:06 +0200

    Update CircleArea.java

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -5,9 +5,9 @@ public class CircleArea implements ISubscriber {
     public void notifySubscriber(String input) {
         System.out.println("Hello, I am Circle Area and I am notified with " + input);
 
-        double radius;
+        int radius;
         try {
-            radius = Double.parseDouble(input);
+            radius = Integer.parseDouble(input);
             if (radius < 0) {
                 System.out.println("Radius is negative.");
                 return;
@@ -21,7 +21,7 @@ public class CircleArea implements ISubscriber {
         System.out.println("Radius is " + radius + ",  Area is: " + calculat(radius));
     }
 
-    public double calculat(double input) {
+    public double calculat(int input) {
         return  Math.PI * input *input;
     }
 }
