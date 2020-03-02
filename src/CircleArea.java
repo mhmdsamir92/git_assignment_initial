f9aec7a997fbda5d08413c2292552574ac6ca59b
Marim-medhat <36182751+Marim-medhat@users.noreply.github.com>
2020-03-02 17:21:29 +0200

    Update CircleArea.java

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -7,7 +7,7 @@ public class CircleArea implements ISubscriber {
 
         int radius;
         try {
-            radius = Integer.parseDouble(input);
+            radius = Integer.parseInt(input);
             if (radius < 0) {
                 System.out.println("Radius is negative.");
                 return;
