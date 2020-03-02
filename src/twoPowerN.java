275237af330fe8a499984e71f04c00c7f1b328c0
islamadel2000 <eslamadel2000@yahoo.com>
2020-03-02 04:39:11 +0200

    Edit at class twoPowerN 20170048

--- src/twoPowerN.java
+++ src/twoPowerN.java
@@ -9,26 +9,18 @@ public class twoPowerN extends ThreadSubscriber{
 
     @Override
     public String execute() {
-    	int exponent=Integer.parseInteger(input);
-    	  static int twoPowerN(exponent)
-  	    {
-  	    	 int product=2;
-  	   	  
-  	   	  switch (exponent){
-  	   		  case 0 :
-  	   			  product=1;
-  	   			  break;}
-  	   	 
-  	   			for(int i =1;i< exponent;i++)
-  	   			{ 
-  	   				product=product*2;
-  	   				}
-  	   			
-  	   			 return product;
-  	    }
-  	
-  	
-      
-        return String.valueOf(product);
-    }
+    	  int exponent = Integer.parseInt(input);
+		int product = 2;
+
+		switch (exponent) {
+			case 0:
+		        product = 1;
+		        break;
+		}
+
+		for (int i = 1; i < exponent; i++) {
+			product = product * 2;
+		}
+
+		return String.valueOf(product) ;
 }
