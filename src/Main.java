8867a5fa7fb7ecb282d00f325fd2b2de7f494ec6
Abdulazizsayed <Abdulazizsayed>
2020-03-02 21:39:27 +0200

    Modification of class name

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
-                        new TowPowerN(),
+                        new TwoPowerN(),
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
---
+++ src/TwoPowerN.java
@@ -0,0 +1,13 @@
+
+public class TwoPowerN implements ISubscriber {
+	@Override
+	public void notifySubscriber(String input) {
+		int p = Integer.parseInt(input), res = 2;
+
+                for(int i = 1 ; i < p ; i++){
+                    res += res;
+                }
+
+                System.out.println(res);
+	}
+}
