f8cca03666de08b6e82df1e19fea43c53be058a3
oali88 <45426880+oali88@users.noreply.github.com>
2020-03-02 11:28:33 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
+		        new summtionSeries(),
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
