a6e107542ee8fe1486b93e4d4d524b1d2d6440b3
halahamde <36208722+halahamde@users.noreply.github.com>
2020-03-03 01:17:46 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 			new ReallySimpleSubscriber(),
 			new SphereVolumeSubscriber(),
 			new MultiplicationSeries(),
+		         new SummationSeries(),
 			new Circlecir()
 	};
 	public static void main(String[] args) {
