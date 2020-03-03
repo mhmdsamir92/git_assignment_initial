680c025bb3a0f8abd4c22d8a976ca8c942ecd91c
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-03-03 11:58:03 +0200

    List for user to choose from

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,75 @@ public class Main {
 		new MultSer(),
 		new CircleVolume(),
     	new SphereVolume(),
-  };
+  	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
 			mathTopic.addSubscriber(sub);
 		}
+		showList();
 		Scanner sc = new Scanner(System.in);
+		int choice = sc.nextInt();
+		System.out.println("Enter value: ");
 		String input = sc.next();
-		mathTopic.dispatchEvent(input);
 		sc.close();
+		if(choice == 0) 
+			mathTopic.dispatchEvent(input);
+		else
+			mathOP(input, choice - 1);
+		
+	}
+	public static void showList(){
+		String choices = "1-Sphere Area\n"
+						+"2-Circle Area\n"
+						+"3-SummationSeries\n"
+						+"4-Lucas Series\n"
+						+"5-Fibonacci\n"
+						+"6-Sphere Circumference\n"
+						+"7-Multiplication series\n"
+						+"8-Circle Volume\n"
+						+"9-Sphere Volume\n"
+						+"0-For all\n";
+		System.out.println(choices);
+	}
+	public static void mathOP(String input, int choice){
+		switch(choice){
+			case 1:
+				subscribers[1].notifySubscriber(input);
+				break;
+			
+			case 2:
+				subscribers[2].notifySubscriber(input);
+				break;
+			
+			case 3:
+				subscribers[3].notifySubscriber(input);
+				break;
+			
+			case 4:
+				subscribers[4].notifySubscriber(input);
+				break;
+			
+			case 5:
+				subscribers[5].notifySubscriber(input);
+				break;
+			
+			case 6:
+				subscribers[6].notifySubscriber(input);
+				break;
+			
+			case 7:
+				subscribers[7].notifySubscriber(input);
+				break;
+			
+			case 8:
+				subscribers[8].notifySubscriber(input);
+				break;
+			
+			case 9:
+				subscribers[9].notifySubscriber(input);
+				break;			
+		}
+
 	}
 }
