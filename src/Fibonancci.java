ee0bbb56ebdaa0bd5ab83cb32b75a63dc149de76
islamadel2000 <eslamadel2000@yahoo.com>
2020-03-03 22:00:33 +0200

    gobran branch 20160389

--- src/Fibonancci.java
+++ src/Fibonancci.java
@@ -1,26 +1,24 @@
-
-
-
-class Fibonacci implements ISubscriber{
-
-    void printFibonacci(int count){    
-    int n1=0,n2=1,n3,i;    
-        System.out.println("the Fibonacci for " + count);
-    System.out.print(n1+" "+n2);//printing 0 and 1    
-
-    for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
-    {    
-     n3=n1+n2;    
-     System.out.print(" "+n3);    
-     n1=n2;    
-     n2=n3;    
-    }    
- }    
-
-
-    @Override
-    public void notifySubscriber(String input) {
-        int n = Integer.parseInt(input);
-       printFibonacci(n);
-    }
+public class Fibonacci extends ThreadSubscriber{
+double input;
+	@Override
+	public void notifySubscriber() {
+		input = topic.getInput();
+		System.out.println("Result of Fibonacci series at "+input+": "+execute());
+	}
+	@Override
+	public String execute() {
+		int num1 = 0;
+		int num2 = 1;
+		int sum = 0;
+		if(input<=1)
+			sum = (int)input;
+		for(int i=1 ; i < input ; i++) {
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+		}
+		String str = String.valueOf(sum);
+		
+	    return str;
+	}
 }
