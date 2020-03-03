68373d484d7e6b9351642c9b6c4b454782a50dbe
OmarAshrafLabib99 <44038598+OmarAshrafLabib99@users.noreply.github.com>
2020-03-03 18:56:15 +0200

    add menu

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
+			
 			new SphereVolume(),
 			new AreaSphere(),
-			new Power(),
 			new LucasSeries(),
 			new CalculateArea(),
-			new Fibonacci(),
-			
-      new CalcBallVolume()
+			new Fibonacci(),	
+			new CalcBallVolume()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
@@ -20,7 +17,42 @@ public class Main {
 			mathTopic.addSubscriber(sub);
 		}
 		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
-		mathTopic.dispatchEvent(input);
+		
+		int choice;
+		String input;
+		while(true) 
+		{
+			System.out.print(
+					  "1-SphereVolume\n" + 
+					  "2-CircleVolume\n" +
+					  "3-lucasSeries\n"+
+					  "4-SphereArea\n"+
+					  "5-CircleArea\n"+
+					  "6-fibonacci\n"+
+					  "7-execute all\n"+
+					  "8-terminate\n");
+			choice = sc.nextInt();
+			switch (choice) 
+			{
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+				System.out.print("Enter input: ");
+				input = sc.next();
+				mathTopic.dispatchEvent(input);
+				break;
+			case 8:
+				System.exit(0);
+				break;				
+			default:
+				System.out.println("wrong choice");
+				break;
+			}
+			System.out.println();
+		}
 	}
 }
