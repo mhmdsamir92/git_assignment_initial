58a4e5a00825f4a2d12ab267c59c4302b1cf3acd
Amr Magdy <36246549+amrmagdy12@users.noreply.github.com>
2020-03-03 23:08:04 +0200

    20170188-Fibonacci-Bonusadded
    Updated with implemented methods for IntegerSubscriber..

---
+++ src/company/Fibonacci.java
@@ -0,0 +1,51 @@
+package company;
+public class Fibonacci extends IntegerSubscriber{
+
+	@Override
+	public void preprocess(int input) throws Exception {
+
+		if(input < 1 ) throw new Exception("0 and Negative values are invalid ") ;
+
+		System.out.println("I'am Fibonnacii and I'am notified with >> " + input);
+	}
+
+	@Override
+	public int doCalculation(int n) {
+		int pascTriangle[][]  =  new int [n][n] ;
+		pascTriangle[1][0] = 1;
+		pascTriangle[1][1] = 1;
+
+		if (n == 1 || n == 2) return 1 ;
+		else {
+			// drawing pascal's triangle as a base for Fibonnacii
+			for (int i = 2; i < n; i++) {
+				pascTriangle[i][0] = 1;
+				for (int j = 1; j <= i; j++) {
+					pascTriangle[i][j] = pascTriangle[i - 1][j - 1] + pascTriangle[i - 1][j];
+				}
+				pascTriangle[i][i] = 1;
+			}
+
+			int i = n - 1;
+			int j = 0;
+			int result = 0;
+
+			while (j <= i)
+			{
+				result += pascTriangle[i][j];
+				i--;
+				j++;
+
+			}
+
+			return result ;
+
+		}
+	}
+
+	@Override
+	public void printAnswer(int out) {
+
+		System.out.println("the answer is " + out );
+	}
+}
