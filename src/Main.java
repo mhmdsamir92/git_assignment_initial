5ee3bd52fb1e7d3321937c0de81cc74aa86c30d6
Amr Magdy <36246549+amrmagdy12@users.noreply.github.com>
2020-03-03 23:49:07 +0200

    Updated-with-bonus

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-package com.company;
+package company;
 
 import java.util.Scanner;
 
 public class Main {
-	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
+	private static IntegerSubscriber subscribers [] = {
 			new Fibonacci(),
 	};
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
 			mathTopic.addSubscriber(sub);
