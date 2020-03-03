0f5a4c6c49972c0dcdd6732fb9ba7df280d93066
Mina Botros <36329091+Mina1998@users.noreply.github.com>
2020-03-03 20:56:52 +0200

    Circle Volume Bonus

--- src/CircleVolume.java
+++ src/CircleVolume.java
@@ -1,11 +1,17 @@
-
-public class CircleVolume implements ISubscriber {
-
+public class CircleVolume extends DoubleSubscriber {
 	@Override
-	public void notifySubscriber(String input) {
-		int radius = Integer.parseInt(input);
-		double CircleVolume = (4.0/3.0) * Math.PI * Math.pow(radius, 3);
-		System.out.println("The Volume of the Circle is : " + CircleVolume);
-	}
+    public void preProcess(int input) throws Exception {
+        System.out.print("Hello, I'm CircleVolume, and I'm notified with: " + input + " as radius. ");
+        if (input < 0) throw new Exception("Negative radius is invalid!");
+    }
+
+    public double doCalculation(int input) {
+        return (4.0 / 3.0) * Math.PI * Math.pow(input, 3);
+    }
+
+    @Override
+    public void printAnswer(double output) {
+        System.out.println("Circle Volume is: " + output);
+    }
 
 }
