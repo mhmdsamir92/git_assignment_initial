168c6f5bdfe7f37ccff6c942ad4d1889fbb6da87
mohamed magdy <36678529+devmagdyy@users.noreply.github.com>
2020-03-03 23:05:13 +0200

    20160219-MultiplicationSeries-Adding-Bonus
    Adding Bonus

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -1,17 +1,23 @@
-public class MultiplicationSeries implements ISubscriber {
-
-    public int m(String n) {
-        int x = Integer.parseInt(n);
+public class Multiplicationseriess implements IntegerSubscriber {
+    public int m(int n){
         int result = 1;
-        for (int i = 1; i <= x; i++) {
-            result = result * i;
+        int x = Integer.parseInt(n);
+        for(int i=1; i<=x; i++){
+            result = result*i;
         }
         return result;
     }
-
     @Override
-    public void notifySubscriber(String input) {
-        System.out.print("Hello,I am a multiplication series and  I am notified with" + input);
-        System.out.print("and my result is : " + m(input));
+    public void preProcess(int input) {
+        System.out.println("Hello, I'm MultiplicationSeries, and I'm notified with: " + input + " as input. ");
+        if (input < 0) throw new Exception("Negative input is invalid!");
+    }
+    @Override
+    public int doCalculation(int input) {
+        return m(input);
     }
 }
+    @Override
+    public void printAnswer(int output) {
+        System.out.println("The Multiplication Series = " + output);
+    }
