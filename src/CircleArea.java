fa1a1ea24222f49182f53e982e930f437be48823
Marim-medhat <36182751+Marim-medhat@users.noreply.github.com>
2020-03-03 21:58:15 +0200

    Update CircleArea.java

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -1,27 +1,17 @@
+public class CircleArea extends DoubleSubscriber { 
+ @Override
+    public void preProcess(int input) throws Exception {
+        System.out.print("Hello, I'm CircleArea , and I'm notified with: " + input + " as radius. ");
+        if (input < 0) throw new Exception("Negative radius is invalid!"); }
 
-public class CircleArea implements ISubscriber {
 	
-	@Override
-    public void notifySubscriber(String input) {
-        System.out.println("Hello, I am Circle Area and I am notified with " + input);
-
-        int radius;
-        try {
-            radius = Integer.parseInt(input);
-            if (radius < 0) {
-                System.out.println("Radius is negative.");
-                return;
-            }
-
-        } catch (NumberFormatException ignored) {
-            System.out.println("Radius is not a number.");
-            return;
-        }
-
-        System.out.println("Radius is " + radius + ",  Area is: " + calculat(radius));
+	 @Override
+    public double doCalculation(int input) {
+       return  Math.PI * input *input;
     }
 
-    public double calculat(int input) {
-        return  Math.PI * input *input;
-    }
+    @Override
+    public void printAnswer(double output) {
+       System.out.println("Circle Area is: " + output);
+   }   
 }
