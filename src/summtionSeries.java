c8963b8b2c157153e956550a6b14285a1283e07d
oali88 <45426880+oali88@users.noreply.github.com>
2020-03-05 08:55:28 -0800

    Update summtionSeries.java

--- src/summtionSeries.java
+++ src/summtionSeries.java
@@ -5,7 +5,7 @@ public class summtionSeries implements ISubscriber {
         for (int i = 0; i <= x; i++) {
             sum += i;
         }
-        system.out.println(sum);
+        System.out.println(sum);
         //20160152 ali osama ali
 
     }
