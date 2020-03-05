7ea0a0354ca73a2baff9a076ca42b4280c1949e7
Megadardery <Megadardery@yahoo.com>
2020-03-05 23:26:42 +0200

    merge-main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-	private static ISubscriber subscribers [] = {
+    private static ISubscriber subscribers [] = {
+            new MultiplicationSeries(),
+			new SummationSeries(),
+			
+			new CircleCircumference(),
+			new CircleArea(),
+            new CircleVolume(),
+			
+            new SphereCircumference(),
+			new SphereArea(),
+            new SphereVolume(),
+            
 			new Fibonacci(),
-	};
-	public static void main(String[] args){
-		Topic mathTopic = new Topic();
-		for (ISubscriber sub : subscribers) {
-			mathTopic.addSubscriber(sub);
-		}
-		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt() ;
-		mathTopic.dispatchEvent(input);
-	}
+            new TwoPowerN(),
+            
+    };
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Topic mathTopic = new Topic();
+        for (ISubscriber sub : subscribers) {
+            mathTopic.addSubscriber(sub);
+        }
+
+        try{
+            for (int i = 0; i < subscribers.length; ++i) {
+                System.out.println((i+1) + " - " + subscribers[i].getClass().getName());
+            }
+            System.out.println("0 - Run all functions");
+
+            System.out.println("Choose your option:");
+            int choice = Integer.parseInt(sc.next());
+
+            System.out.println("Enter input number: ");
+            int n = Integer.parseInt(sc.next());
+
+            if (choice < 0 || choice > subscribers.length) throw new Exception("Invalid choice.");
+            if (choice != 0)
+                subscribers[choice-1].notifySubscriber(n);
+            else
+                mathTopic.dispatchEvent(n);
+        }catch(NumberFormatException ignored) {
+            System.out.println("Your input is not a valid integer.");
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
